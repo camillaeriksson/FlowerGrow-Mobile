1dc99ae047a1bbf116a9c93ab4d107610468a902
Lisa <lisa.bengtsson@live.com>
2020-12-22 10:30:15 +0100

    render the background img as style in app

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require('./assets/background.png')} style= {styles.backgroundImage}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
+  backgroundImage:  {
+    flex: 1,
+    resizeMode: 'cover',
+  }
 });
