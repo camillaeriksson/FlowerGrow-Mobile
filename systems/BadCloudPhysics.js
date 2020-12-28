f891d9b922614dc4dcec79202cf89c5c6772139d
Camilla Eriksson <ericsson.camilla@gmail.com>
2020-12-28 11:55:42 +0100

    Broke out physics for bad cloud

---
+++ systems/BadCloudPhysics.js
@@ -0,0 +1,18 @@
+import Matter from 'matter-js'
+import { Dimensions } from 'react-native';
+
+const max_height = Dimensions.get('screen').height;
+const max_width = Dimensions.get('screen').width;
+
+const BadCloudPhysics = (entities, { time }) => {
+  let engine = entities.physics.engine;
+  let badCloud = entities.badCloud.body;
+
+  Matter.Body.translate(badCloud, { x: 0, y: 1 })
+
+  Matter.Engine.update(engine, time.delta); 
+
+  return entities;
+}
+
+export default BadCloudPhysics;
