b88577e383829d90c453deefd1b754bbcbee3e25
Lisa <lisa.bengtsson@live.com>
2020-12-28 12:26:20 +0100

    implemeted new separation of Physic-files to flowr

--- systems/FlowerPhysics.js
+++ systems/FlowerPhysics.js
@@ -1,16 +1,13 @@
 import Matter from 'matter-js'
 import { Dimensions } from 'react-native';
 
-const max_height = Dimensions.get('screen').height;
 const max_width = Dimensions.get('screen').width;
 
-const Physics = (entities, { touches, time }) => {
+const FlowerPhysics = (entities, { touches, time }) => {
   let engine = entities.physics.engine;
-  let badCloud = entities.badCloud.body;
   let flower = entities.flower.body;
 
   touches.filter(t => t.type === 'press').forEach(t => {
-    console.log('HEJ', t.event.locationX)
     if (t.event.locationX < max_width / 2) {
       Matter.Body.translate(flower, { x: -5, y: 0 });
     } else if (t.event.locationX > max_width / 2) {
@@ -18,11 +15,9 @@ const Physics = (entities, { touches, time }) => {
     }
   });
 
-  Matter.Body.translate(badCloud, { x: 0, y: 1 });
-
   Matter.Engine.update(engine, time.delta); 
 
   return entities;
 }
 
-export default Physics;
+export default FlowerPhysics;
