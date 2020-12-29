33f3405698a29b2c08c0bbd6dc78b8689460031f
Lisa <lisa.bengtsson@live.com>
2020-12-29 14:01:43 +0100

    added flower_100 to new images.js

--- assets/Images.js
+++ assets/Images.js
@@ -2,4 +2,5 @@ export default Images = {
   badCloud1: require('./bad_cloud1.png'),
   badCloud2: require('./bad_cloud2.png'),
   badCloud3: require('./bad_cloud3.png'),
+  flower_100: require('./flower_100.png')
 }
