ff715e6d371c11a17b4c4ac7447ab9a428936d06
Lisa <lisa.bengtsson@live.com>
2020-12-29 11:29:30 +0100

    made check for flower to stay in screen

--- systems/FlowerPhysics.js
+++ systems/FlowerPhysics.js
@@ -2,18 +2,29 @@ import Matter from 'matter-js';
 import { Dimensions } from 'react-native';
 
 const max_width = Dimensions.get('screen').width;
+const min_width = Dimensions.get('screen').width - Dimensions.get('screen').width;
 
 const FlowerPhysics = (entities, { touches, time }) => {
   let engine = entities.physics.engine;
   let flower = entities.flower.body;
 
+  
+  // Function for the touch movement of the flower
   touches.filter(t => t.type === 'move').forEach(t => {
+    const flowerRadius = 75;
+
     if (t.event.locationX < max_width / 2) {
-      Matter.Body.translate(flower, { x: -5, y: 0 });
+      Matter.Body.translate(flower, { x: -3, y: 0 });
     } else if (t.event.locationX > max_width / 2) {
-      Matter.Body.translate(flower, { x: 5, y: 0 });
+      Matter.Body.translate(flower, { x: +3, y: 0 });
+    } if (flower.position.x > max_width - flowerRadius) {
+      flower.position.x = max_width - flowerRadius;
+    } if (flower.position.x < min_width) {
+      flower.position.x = min_width;
     }
+    return flower.position.x;
   });
+
 
   Matter.Engine.update(engine, time.delta); 
 
