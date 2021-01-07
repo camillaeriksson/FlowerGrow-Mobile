1edf1d44f8eb543e7cb2c3d3deadb2677172f886
Lisa <lisa.bengtsson@live.com>
2021-01-07 10:32:04 +0100

    cleaned code from commented-out code

--- GameArea.js
+++ GameArea.js
@@ -10,7 +10,6 @@ import { Dimensions } from 'react-native';
 import Grass from './components/Grass';
 import Pot from './components/Pot';
 import Flower from './components/Flower';
-// import WaterMeter from './components/WaterMeter';
 import Test from './components/Test';
 import WaterMeterBackground from './components/WaterMeterBackground'
 
@@ -39,7 +38,6 @@ export default class GameArea extends Component {
     let grass = Matter.Bodies.rectangle(0, max_height - 150, max_width, 150, { isStatic: true });
     let pot = Matter.Bodies.rectangle(max_width / 2 - 50, max_height - 140, 100, 80, { isStatic: true });
     let flower = Matter.Bodies.circle(max_width / 2 - 38 , max_height / 2, 76, { isStatic: true });
-    // let waterMeter = Matter.Bodies.rectangle(20, max_height - 300, 30, 170, { isStatic: true });
     //let badCloud1 = Matter.Bodies.rectangle(this.randomizeXpos(0, max_width), -30, 117, 60, {isStatic: true });
     // let badCloud2 = Matter.Bodies.rectangle(this.randomizeXpos(0, max_width), -30, 117, 60, {isStatic: true });
     let test = Matter.Bodies.rectangle(100, max_height - 800, 50, 50, { isSensor: true });
@@ -98,7 +96,6 @@ export default class GameArea extends Component {
       // badCloud1: { body: badCloud1, size: [117, 60], renderer: BadCloud},
       // badCloud2: { body: badCloud2, size: [117, 60], renderer: BadCloud},
       flower: { body: flower, color: 'blue', size: [76], renderer: Flower},
-      // waterMeter: { body: waterMeter, color: 'blue', size: [30, 170], renderer: WaterMeter},
       test: { body: test, color: 'red', size: [50, 50], renderer: Test},
       //test2: { body: test2, color: 'blue', size: [50, 50], renderer: Test},
       waterMeterBackground: { body: waterMeterBackground, color: 'grey', size: [30, 160], renderer: WaterMeterBackground}
