408c6fa6b635b72680ba1e585e22f2c3b98c46ae
Lisa <lisa.bengtsson@live.com>
2021-01-07 18:07:51 +0100

    calculation for when flwr is out of screen

--- systems/FlowerPhysics.js
+++ systems/FlowerPhysics.js
@@ -7,18 +7,16 @@ const min_width = 0;
 
 const FlowerPhysics = (entities, { touches }) => {
   let flower = entities.flower.body;
+  
   // Function for the touch movement of the flower
   touches.filter(t => t.type === 'move').forEach(t => {
-    const flowerDiameter = 60;
+    const flowerRadius = 30;
       Matter.Body.setPosition(flower, { x: t.event.locationX, y: max_height / 2 });
-    if (flower.position.x - 30 > max_width - 30) {
-      Matter.Body.setPosition(flower, { x: max_width - 30, y: max_height / 2});
-    } if (flower.position.x < min_width) {
-      Matter.Body.setPosition(flower, { x: min_width, y: max_height / 2});
+    if (flower.position.x + flowerRadius > max_width) {
+      Matter.Body.setPosition(flower, { x: max_width - flowerRadius, y: max_height / 2});
+    } if (flower.position.x - flowerRadius < min_width) {
+      Matter.Body.setPosition(flower, { x: min_width + flowerRadius, y: max_height / 2});
     }
-    // console.log('max wodth: ', max_width)
-    // console.log('flower positon: ', flower.position.x)
-    // console.log('touch: ', t.event.locationX)
   });
 
 
