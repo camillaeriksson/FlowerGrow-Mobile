0e9a3923f337da49086b27f835d4555f4b0eb696
Camilla Eriksson <ericsson.camilla@gmail.com>
2021-01-08 10:04:50 +0100

    Made grass and pot static if under screen

--- systems/GroundPhysics.js
+++ systems/GroundPhysics.js
@@ -2,17 +2,16 @@ import Matter from 'matter-js';
 import { Dimensions } from 'react-native';
 
 const max_height = Dimensions.get('screen').height;
+const max_width = Dimensions.get('screen').height;
 
 const GroundPhysics = (entities) => {
   let pot = entities.pot.body;
   let grass = entities.grass.body;
 
-  // Matter.Body.translate(pot, { x: 0, y: 1 });
-  // Matter.Body.translate(grass, { x: 0, y: 1 });
-  if (grass.position.y > max_height + 1000) {
-    grass.position.y = max_height + 1000;
-  } if (pot.position.y > max_height + 1000) {
-    pot.position.y = max_height + 1000;
+  if (grass.position.y > max_height + 100) {
+    Matter.Body.setStatic(grass, true)
+  } if (pot.position.y > max_height + 100) {
+    Matter.Body.setStatic(pot, true)
   }
 
   return entities;
