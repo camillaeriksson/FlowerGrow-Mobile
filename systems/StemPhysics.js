6b6ac67f9444725983b8b9de5f6c738d36917225
Lisa <lisa.bengtsson@live.com>
2021-01-13 15:48:45 +0100

    cleaned code

--- systems/StemPhysics.js
+++ systems/StemPhysics.js
@@ -1,63 +1,10 @@
-import Matter, { Constraint, World } from 'matter-js';
-import { Dimensions } from 'react-native';
-import Stem from '../components/Stem';
-
-//var Engine = Matter.Engine;
-
-const max_height = Dimensions.get('screen').height;
-const max_width = Dimensions.get('screen').width;
-
-/* const createStemPoints = (world, entities) => {
-
-  let stemPoint = Matter.Bodies.rectangle(max_width / 2, max_height / 2 + 250, 3, 350);
-
-  Matter.World.add(world, [stemPoint]);
-  
-  
-  entities['stemPoint'] = {
-    body: stemPoint,
-    color: 'green',
-    size: [3, 350],
-    renderer: Stem
-  } 
-
-} */
+import Matter from 'matter-js';
 
 const StemPhysics = (entities) => {
-  let engine = entities.physics.engine;
   let flower = entities.flower.body;
   let stem = entities.stem.body;
-  let stemHeight = max_height - flower.position.y
-
-  stem.stemHeight = stemHeight
 
   Matter.Body.setPosition(stem, {x: flower.position.x, y: flower.position.y + 400});
-
-  //console.log('flower', max_height - flower.position.y, 'stemheight', stemHeight)
-
-  //createStemPoints(world, entities)
-
-  /* var options = {
-    bodyA: stem,
-    bodyB: flower,
-    pointA: {x: 0, y: 0},
-    pointB: {x: 0, y: 170},
-    stiffness: 1,
-    length: 0,
-  }
-
-  var constraint = Constraint.create(options);
-  World.add(engine.world, constraint); */
-  
-
-  /* Object.keys(entities).forEach(key => {
-    if (key.indexOf('stemPoint') === 0) {
-      Matter.Body.setPosition(entities[key].body, {
-        x: flower.position.x, 
-        y: max_height / 2 + 250
-      });
-    }
-  }); */
 
   return entities;
 
