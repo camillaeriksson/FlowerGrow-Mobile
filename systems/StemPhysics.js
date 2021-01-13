9a13ff3a03b9ad06549bd2252647fb251d4cf8d9
Lisa <lisa.bengtsson@live.com>
2021-01-13 15:51:29 +0100

    added comment

--- systems/StemPhysics.js
+++ systems/StemPhysics.js
@@ -4,6 +4,8 @@ const StemPhysics = (entities) => {
   let flower = entities.flower.body;
   let stem = entities.stem.body;
 
+  // Make the stem keep the position with the flower 
+  //and grow up with it on start
   Matter.Body.setPosition(stem, {x: flower.position.x, y: flower.position.y + 400});
 
   return entities;
