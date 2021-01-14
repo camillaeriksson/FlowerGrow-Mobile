d583164b869de60826f1b5fd626939a3dae69ca4
Camilla Eriksson <ericsson.camilla@gmail.com>
2021-01-14 10:05:06 +0100

    Comments in WaterMeterPhysics

--- systems/WaterMeterPhysics.js
+++ systems/WaterMeterPhysics.js
@@ -1,10 +1,14 @@
 const WaterMeterPhysics = (entities, onEvent) => {
+  // Checking if there is an event
   if (onEvent.events.length) {
+    // Reduce the water level and move it downwards if the dispatched event is "score down"
     if (onEvent.events[0].type === 'score_down') {
       entities.waterMeter.waterLevel -= 32;
       entities.waterMeter.newWaterMeterY +=32;
     } 
+    // Increase the water level and move it upwards if the dispatched event is "score up"
     if (onEvent.events[0].type === 'score_up') {
+      // Only if the water level is not full (160 px high)
       if (entities.waterMeter.waterLevel < 160) {
         entities.waterMeter.waterLevel += 32;
         entities.waterMeter.newWaterMeterY -= 32;
