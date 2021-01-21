397570170218188bd92d0d5f60cdea6e9d62bd74
Lisa <lisa.bengtsson@live.com>
2021-01-21 17:24:04 +0100

    changed styling on game over screen

--- components/GameOverScreen.js
+++ components/GameOverScreen.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import { ImageBackground, Text, StyleSheet, Image } from 'react-native';
+import { Dimensions } from 'react-native';
+import { ImageBackground, Text, StyleSheet, Image, View } from 'react-native';
 import Images from '../assets/Images';
+
+const max_height = Dimensions.get('screen').height;
+const max_width = Dimensions.get('screen').width;
 export default class Grass extends Component {
   render() {
     return (
       <ImageBackground source={Images['background']} style={styles.fullScreen}>
         <Image source={Images['flower_0']} style={styles.flower_0} />
-        <Text style={styles.gameOverText}>GROW OVER!</Text>
-        <Text style={styles.scoreText}>You grew {this.props.score} meter</Text>
+        <View style={styles.textContainer}>
+          <Text style={styles.gameOverText}>GROW OVER!</Text>
+          <Text style={styles.scoreText}>You grew {this.props.score} meter</Text>
+        </View>
       </ImageBackground>
     ) 
   } 
@@ -20,21 +26,34 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     right: 0,
-    justifyContent: 'center',
     alignItems: 'center'
   },
   gameOverText: {
-    color: 'white',
+    color: '#AC722A',
     fontSize: 42,
     textAlign: 'center'
   },
   scoreText: {
     color: 'white',
-    fontSize: 36,
+    fontSize: 30,
     textAlign: 'center'
   },
   flower_0: {
+    position: 'absolute',
+    top: max_height / 3 - 60,
     width: 108,
-    height: 112.5
+    height: 112.5,
+    zIndex: 1
+  },
+  textContainer: {
+    position: 'absolute',
+    top: max_height / 3,
+    borderColor: '#9B4C1B',
+    borderWidth: 5,
+    borderRadius: 10,
+    paddingTop: 50,
+    paddingBottom: 40,
+    padding: 35,
+    backgroundColor: '#A8E5FC'
   }
 })
